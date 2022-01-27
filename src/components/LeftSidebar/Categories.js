b538d4d4f1b0aaccec4ab479f984e3766514bc83
AradhanaChhabra <aradhanachhabra03@gmail.com>
2022-01-27 20:53:26 +0530

    category state added

--- src/components/LeftSidebar/Categories.js
+++ src/components/LeftSidebar/Categories.js
@@ -1,32 +1,38 @@
-import React from 'react';
+import React,{useState} from 'react';
 import styles from './leftSide.module.css'
 import upArrow from '../../icons/upArrow.svg'
 
 export const Categories = () => {
+    const [categoryQuery, setCategoryQuery] = useState("");
+    const changeCategory = (e) => {
+        setCategoryQuery(e.target.value);
+        console.log(e.target.value);
+    }
     return <div className={styles.categories}>
         <div className={styles.heading}>
             <div>Categories</div>
             <img className={styles.arrowImg} src={upArrow} alt="arrow icon" />
         </div>
+        {/* options */}
         <div className={styles.categoriesOptions}>
             <label className={styles.ctgLabels} key={1}>
                 Flip Flops
-                <input className={styles.ctgOpt} type="radio" name="ctgOpt" value="flipFlops" />
+                <input className={styles.ctgOpt} type="radio" name="ctgOpt" value="flipFlops" onChange={changeCategory} />
                 <span className={styles.checkmark}></span>
             </label>
             <label className={styles.ctgLabels} key={2}>
                 Sneakers
-                <input className={styles.ctgOpt} type="radio" name="ctgOpt" value="sneakers" />
+                <input className={styles.ctgOpt} type="radio" name="ctgOpt" value="sneakers" onChange={changeCategory}/>
                 <span className={styles.checkmark}></span>
             </label>
             <label className={styles.ctgLabels} key={3}>
                 Lace-up Shoes
-                <input className={styles.ctgOpt} type="radio" name="ctgOpt" value="laceUpShoes" />
+                <input className={styles.ctgOpt} type="radio" name="ctgOpt" value="laceUpShoes" onChange={changeCategory}/>
                 <span className={styles.checkmark}></span>
             </label>
             <label className={styles.ctgLabels} key={4}>
                 Shoe Accessories
-                <input className={styles.ctgOpt} type="radio" name="ctgOpt" value="shoeAccessories" />
+                <input className={styles.ctgOpt} type="radio" name="ctgOpt" value="shoeAccessories" onChange={changeCategory}/>
                 <span className={styles.checkmark}></span>
             </label>
 
